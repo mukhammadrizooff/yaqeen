308db7fb10ede8241733a97270581c6a49b846e4
mukhammadrizooff <legend.it@bk.ru>
2024-04-30 21:58:12 +0500

    Fix shocase errors

--- constants/showcase.js
+++ constants/showcase.js
@@ -5,13 +5,13 @@ export const showCase = [
     shortDescription:
       "Uzbekiston respublikasi, Andijon viloyatining 2-kichik dahasida qad rostlagan 700 talik xonadonlarimiz.",
     description:
-      "Karachi: Karachi is the largest and most populous city in Pakistan, located on the southern coast of the country. It is known as the financial and economic hub of Pakistan, with a bustling metropolitan atmosphere. Karachi is home to a diverse population, vibrant markets, and a lively food scene. It also boasts beautiful beaches along the Arabian Sea, historical landmarks, and a thriving arts and entertainment scene.",
-    coverImage: "/Project-showcase/Karachi/Dolmen-bahria-towers.jpg",
+      "Andijonda qad rostlagan Orient Gold Invest kop qavvatlik turar joy majmuamiz o'z bag'riga 700 ta oilani sig'dra oladi hamda uhsbu turar joy majmua ozing zamonaviy tashqi dizayn hamda markazda joylashgan o'ni bilan ham yaqqol ko'zga tashlanadi. Ush bu majmuaga juda ham sifatli hamda mashhur bo'lgan qurilish materiallari va texnologiyalardan foydalangan holda katta tajribali memor va pudratchilarning malaka va konikmalari asosida barpo etilgandur.",
+    coverImage: "https://yaqeenuz.files.wordpress.com/2024/04/5.jpg",
     moreImages: [
-      "/Project-showcase/Karachi/Street-view.jpg",
-      "/Project-showcase/Karachi/City-view.jpg",
-      "/Project-showcase/Karachi/Kharadar-street.jpg",
-      "/Project-showcase/Karachi/Flyover-view.jpg",
+      "https://yaqeenuz.files.wordpress.com/2024/04/4.jpg",
+      "https://yaqeenuz.files.wordpress.com/2024/04/13-1.jpg",
+      "https://yaqeenuz.files.wordpress.com/2024/04/10.jpg",
+      "https://yaqeenuz.files.wordpress.com/2024/04/1.jpg",
     ],
     prevHref: "/projects/6",
     nextHref: "/projects/2",
@@ -22,13 +22,13 @@ export const showCase = [
     shortDescription:
       "Uzbekiston respublikasi, Andijon viloyatining 2-kichik dahasida qad rostlagan 700 talik xonadonlarimiz.",
     description:
-      "Islamabad: Islamabad is the capital city of Pakistan, nestled in the Margalla Hills in the northern part of the country. It is a modern and planned city, known for its wide, tree-lined avenues, spacious parks, and serene beauty. Islamabad is home to the country's government institutions, diplomatic enclaves, and a growing technology and business sector. The city also offers a range of recreational activities, including hiking trails, golf courses, and cultural festivals.",
-    coverImage: "/Project-showcase/Islamabad/Faisal-mosque.jpg",
+      "Andijonda qad rostlagan Orient Gold Invest kop qavvatlik turar joy majmuamiz o'z bag'riga 700 ta oilani sig'dra oladi hamda uhsbu turar joy majmua ozing zamonaviy tashqi dizayn hamda markazda joylashgan o'ni bilan ham yaqqol ko'zga tashlanadi. Ush bu majmuaga juda ham sifatli hamda mashhur bo'lgan qurilish materiallari va texnologiyalardan foydalangan holda katta tajribali memor va pudratchilarning malaka va konikmalari asosida barpo etilgandur.",
+    coverImage: "https://yaqeenuz.files.wordpress.com/2024/04/5.jpg",
     moreImages: [
-      "/Project-showcase/Islamabad/Fatima-Jinnah-Park.jpg",
-      "/Project-showcase/Islamabad/Lotus-temple.jpg",
-      "/Project-showcase/Islamabad/Margala-hills.jpg",
-      "/Project-showcase/Islamabad/Islamabad-murre-highway.jpg",
+      "https://yaqeenuz.files.wordpress.com/2024/04/4.jpg",
+      "https://yaqeenuz.files.wordpress.com/2024/04/13-1.jpg",
+      "https://yaqeenuz.files.wordpress.com/2024/04/10.jpg",
+      "https://yaqeenuz.files.wordpress.com/2024/04/1.jpg",
     ],
     prevHref: "/projects/1",
     nextHref: "/projects/5",
@@ -39,13 +39,13 @@ export const showCase = [
     shortDescription:
       "Uzbekiston respublikasi, Andijon viloyatining 2-kichik dahasida qad rostlagan 700 talik xonadonlarimiz.",
     description:
-      "Lahore: Lahore is the cultural capital of Pakistan, situated in the eastern province of Punjab. It is renowned for its rich history, Mughal-era architecture, and vibrant traditions. Lahore is home to many historical sites, including the iconic Badshahi Mosque and Lahore Fort. The city is also known for its love of arts, literature, and music, with numerous festivals and cultural events taking place throughout the year.",
-    coverImage: "/Project-showcase/Lahore/Badshahi-mosque.jpg",
+      "Andijonda qad rostlagan Orient Gold Invest kop qavvatlik turar joy majmuamiz o'z bag'riga 700 ta oilani sig'dra oladi hamda uhsbu turar joy majmua ozing zamonaviy tashqi dizayn hamda markazda joylashgan o'ni bilan ham yaqqol ko'zga tashlanadi. Ush bu majmuaga juda ham sifatli hamda mashhur bo'lgan qurilish materiallari va texnologiyalardan foydalangan holda katta tajribali memor va pudratchilarning malaka va konikmalari asosida barpo etilgandur.",
+    coverImage: "https://yaqeenuz.files.wordpress.com/2024/04/5.jpg",
     moreImages: [
-      "/Project-showcase/Lahore/Minar-e-pakistan.jpg",
-      "/Project-showcase/Lahore/Royal-fort.jpg",
-      "/Project-showcase/Lahore/Rose-garden-fc.jpg",
-      "/Project-showcase/Lahore/Walled-city.jpg",
+      "https://yaqeenuz.files.wordpress.com/2024/04/4.jpg",
+      "https://yaqeenuz.files.wordpress.com/2024/04/13-1.jpg",
+      "https://yaqeenuz.files.wordpress.com/2024/04/10.jpg",
+      "https://yaqeenuz.files.wordpress.com/2024/04/1.jpg",
     ],
     prevHref: "/projects/2",
     nextHref: "/projects/4",
@@ -56,13 +56,13 @@ export const showCase = [
     shortDescription:
       "Uzbekiston respublikasi, Andijon viloyatining 2-kichik dahasida qad rostlagan 700 talik xonadonlarimiz.",
     description:
-      "Quetta: Quetta is the provincial capital of Balochistan, located in southwestern Pakistan. It is surrounded by rugged mountains and serves as a gateway to the famous Bolan Pass. Quetta has a distinctive culture influenced by its proximity to Afghanistan and Iran. The city is known for its dry, arid climate and serves as a trading center for fruits, particularly apples. Quetta is also a starting point for exploring the scenic beauty of the province, including the Ziarat Valley and Hanna Lake.",
-    coverImage: "/Project-showcase/Quetta/Quaid-e-azam-residency.jpg",
+      "Andijonda qad rostlagan Orient Gold Invest kop qavvatlik turar joy majmuamiz o'z bag'riga 700 ta oilani sig'dra oladi hamda uhsbu turar joy majmua ozing zamonaviy tashqi dizayn hamda markazda joylashgan o'ni bilan ham yaqqol ko'zga tashlanadi. Ush bu majmuaga juda ham sifatli hamda mashhur bo'lgan qurilish materiallari va texnologiyalardan foydalangan holda katta tajribali memor va pudratchilarning malaka va konikmalari asosida barpo etilgandur.",
+    coverImage: "https://yaqeenuz.files.wordpress.com/2024/04/5.jpg",
     moreImages: [
-      "/Project-showcase/Quetta/Hanna-lake.jpg",
-      "/Project-showcase/Quetta/Miri-fort.jpg",
-      "/Project-showcase/Quetta/Bolan-pass.jpg",
-      "/Project-showcase/Quetta/Koh-e-chiltan.jpg",
+      "https://yaqeenuz.files.wordpress.com/2024/04/4.jpg",
+      "https://yaqeenuz.files.wordpress.com/2024/04/13-1.jpg",
+      "https://yaqeenuz.files.wordpress.com/2024/04/10.jpg",
+      "https://yaqeenuz.files.wordpress.com/2024/04/1.jpg",
     ],
     prevHref: "/projects/3",
     nextHref: "/projects/5",
@@ -73,13 +73,13 @@ export const showCase = [
     shortDescription:
       "Uzbekiston respublikasi, Andijon viloyatining 2-kichik dahasida qad rostlagan 700 talik xonadonlarimiz.",
     description:
-      "Peshawar: Peshawar is a city located in the Khyber Pakhtunkhwa province of Pakistan, close to the country's western border with Afghanistan. It is one of the oldest continuously inhabited cities in the world and has a unique blend of cultures and traditions. Peshawar is known for its bustling bazaars, where one can find traditional handicrafts, spices, and vibrant street food. The city is also a gateway to the scenic beauty of the Khyber Pass and the nearby tribal areas.",
-    coverImage: "/Project-showcase/Peshawar/Baab-e-khyber.jpg",
+      "Andijonda qad rostlagan Orient Gold Invest kop qavvatlik turar joy majmuamiz o'z bag'riga 700 ta oilani sig'dra oladi hamda uhsbu turar joy majmua ozing zamonaviy tashqi dizayn hamda markazda joylashgan o'ni bilan ham yaqqol ko'zga tashlanadi. Ush bu majmuaga juda ham sifatli hamda mashhur bo'lgan qurilish materiallari va texnologiyalardan foydalangan holda katta tajribali memor va pudratchilarning malaka va konikmalari asosida barpo etilgandur.",
+    coverImage: "https://yaqeenuz.files.wordpress.com/2024/04/5.jpg",
     moreImages: [
-      "/Project-showcase/Peshawar/Islamia-college.jpg",
-      "/Project-showcase/Peshawar/Sir-cunningham-clock-tower.jpg",
-      "/Project-showcase/Peshawar/Sethi-house.jpg",
-      "/Project-showcase/Peshawar/Peshawar-museum.jpg",
+      "https://yaqeenuz.files.wordpress.com/2024/04/4.jpg",
+      "https://yaqeenuz.files.wordpress.com/2024/04/13-1.jpg",
+      "https://yaqeenuz.files.wordpress.com/2024/04/10.jpg",
+      "https://yaqeenuz.files.wordpress.com/2024/04/1.jpg",
     ],
     prevHref: "/projects/4",
     nextHref: "/projects/6",
@@ -90,13 +90,13 @@ export const showCase = [
     shortDescription:
       "Uzbekiston respublikasi, Andijon viloyatining 2-kichik dahasida qad rostlagan 700 talik xonadonlarimiz.",
     description:
-      "Murree: Murree is a hill station situated in the Pir Panjal Range of the Himalayas, just outside Islamabad. It is a popular tourist destination known for its cool climate, scenic beauty, and lush green hills. Murree offers breathtaking views, picturesque landscapes, and a pleasant escape from the summer heat of the plains. The town is dotted with colonial-era buildings, bustling markets, and charming guesthouses, making it a favored retreat for locals and tourists alike.",
-    coverImage: "/Project-showcase/Murree/Hills-view.jpg",
+      "Andijonda qad rostlagan Orient Gold Invest kop qavvatlik turar joy majmuamiz o'z bag'riga 700 ta oilani sig'dra oladi hamda uhsbu turar joy majmua ozing zamonaviy tashqi dizayn hamda markazda joylashgan o'ni bilan ham yaqqol ko'zga tashlanadi. Ush bu majmuaga juda ham sifatli hamda mashhur bo'lgan qurilish materiallari va texnologiyalardan foydalangan holda katta tajribali memor va pudratchilarning malaka va konikmalari asosida barpo etilgandur.",
+    coverImage: "https://yaqeenuz.files.wordpress.com/2024/04/5.jpg",
     moreImages: [
-      "/Project-showcase/Murree/Attabad-lake.jpg",
-      "/Project-showcase/Murree/Chair-lifts.jpg",
-      "/Project-showcase/Murree/Jhika-gali.jpg",
-      "/Project-showcase/Murree/Murree-city.jpg",
+      "https://yaqeenuz.files.wordpress.com/2024/04/4.jpg",
+      "https://yaqeenuz.files.wordpress.com/2024/04/13-1.jpg",
+      "https://yaqeenuz.files.wordpress.com/2024/04/10.jpg",
+      "https://yaqeenuz.files.wordpress.com/2024/04/1.jpg",
     ],
     prevHref: "/projects/5",
     nextHref: "/projects/1",
